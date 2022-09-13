38eaea3fc517c2a6f6d3e77e8070bf27646da803
jfjfjbjb <697451yr@163.com>
2022-09-13 17:14:29 +0800

    add getQueryParams & download

--- src/utils/common.js
+++ src/utils/common.js
@@ -19,6 +19,77 @@ export default {
     return new URL(`../assets/${url}`, import.meta.url);
   },
   /**
+   * 获取地址栏参数
+   */
+  getQueryParams(url, name) {
+    let path = url || window.location.href;
+    let match = path.match(/(\?[^/#]*)/gi);
+    let map = {};
+    if (match) {
+      match.forEach((matchItem) => {
+        matchItem
+          .slice(1)
+          .split('&')
+          .map((item) => {
+            let keyVal = item.split('=');
+            map[keyVal[0]] = decodeURIComponent(keyVal[1]);
+          });
+      });
+    }
+    if (name) return map[name];
+    return map;
+  },
+  /**
+   * 下载
+   */
+  download(res) {
+    const contentType = _.get(res, 'headers.content-type').toLowerCase();
+    if (contentType === 'application/json' && !this.isSuccessCode(res)) {
+      let reader = new FileReader();
+      reader.onload = (e) => {
+        try {
+          const realRes = JSON.parse(e.target.result);
+          $loading.hide({ method: 'error', tips: _.get(realRes, 'message') });
+        } catch (e) {
+          console.error(e);
+          $loading.hide({ method: 'error', tips: _.get(res, 'data.message') });
+        }
+      };
+      reader.readAsText(res.data);
+      return;
+    }
+    $loading.hide({ useMessage: true, tips: '下载成功！' });
+    this.downloadByBlob(res);
+  },
+  downloadByBlob(response) {
+    // 提取filename
+    const filename = response.headers['content-disposition']
+      .match(/filename=(.*)/)[1]
+      .replace(new RegExp('"', 'g'), '');
+    // 转换为blob
+    const blob = new Blob(response.data, {
+      type: 'application/octet-stream'
+    });
+    if (typeof window.navigator.msSaveBlob !== 'undefined') {
+      // 兼容ie：以本地方式保存文件
+      window.navigator.msSaveBlob(blob, filename);
+    } else {
+      const blobURL = window.URL.createObjectURL(blob);
+      const tempLink = document.createElement('a');
+      tempLink.style.display = 'none';
+      tempLink.href = blobURL;
+      tempLink.setAttribute('download', filename);
+      if (typeof tempLink.download === 'undefined') {
+        // 兼容某些浏览器不兼容download属性
+        tempLink.setAttribute('target', '_blank');
+      }
+      document.body.appendChild(tempLink);
+      tempLink.click();
+      document.body.removeChild(tempLink);
+      window.URL.revokeObjectURL(blobURL);
+    }
+  },
+  /**
    * 复制到剪切板
    * 这种方式针对pc端，仍然比较好用
    */
