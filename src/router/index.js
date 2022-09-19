c4e84d5cd81c00eaf043e35c674a5e4a7a1a17f4
jfjfjbjb <697451yr@163.com>
2022-09-19 17:37:17 +0800

    add exception page; add jsconfig.json

--- src/router/index.js
+++ src/router/index.js
@@ -24,10 +24,17 @@ const router = createRouter({
       path: '/help',
       name: 'help',
       meta: {
-        desc: '帮助页',
-        navi: ['help']
+        desc: '帮助页'
       },
       component: () => import('../views/help/index.vue')
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'exception',
+      meta: {
+        desc: '异常页'
+      },
+      component: () => import('../views/exception/index.vue')
     }
   ]
 });
---
+++ src/views/exception/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="page-exception">
+    <!-- 后续可扩展错误态 -->
+    <a-result
+      status="404"
+      title="404"
+      sub-title="Sorry, the page you visited does not exist."
+    >
+      <template #extra>
+        <a-button type="primary" @click="toHome">回到首页</a-button>
+      </template>
+    </a-result>
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router';
+const route = useRoute();
+const router = useRouter();
+console.log('Exception pathMatch: ', route.params.pathMatch);
+
+// methods
+function toHome() {
+  router.push({
+    name: 'home'
+  });
+}
+</script>
+
+<style scoped lang="less"></style>
